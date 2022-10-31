a7d0fd1ea2da0049734b46d822f1c08ff498074d
Rizky Rasya <rizkyrasya.m@gmail.com>
2022-10-31 22:11:47 +0700

    update

---
+++ node/code.js
@@ -0,0 +1,17 @@
+/*
+
+Command	Description	Common Options
+init
+Membuat berkas package.json pada project
+[--force|-f|--yes|-y|--scope]
+install <package-name>
+Memasang dan mendaftarkan package pada berkas package.json
+[-P|--save-prod|-D|--save-dev|-O|--save-optional] [-E|--save-exact] [-B|--save-bundle] [--no-save] [--dry-run]
+run-script <command>	Menjalankan perintah yang terdapat pada objek scripts di berkas package.json	[--silent] [-- <args>...]
+uninstall <package-name>
+Menghapus dan mengeluarkan package dari berkas package.json	[-S|--save|-D|--save-dev|-O|--save-optional|--no-save]
+version
+Untuk melihat versi package yang tersedia secara global atau lokal
+[<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease [--preid=<prerelease-id>] | from-git]
+
+*/
