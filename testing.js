9c0b6b487b6988a3d31e75281829b0aa7e459140
Rizky Rasya <rizkyrasya.m@gmail.com>
2022-11-05 18:34:27 +0700

    update

--- testing.js
+++ testing.js
@@ -1,8 +1,15 @@
-try {
-    const arr = [1, 2, 3, 4];
-    for (let i = 0; i <= 4; i++) {
-        console.log(arr[i]);
-    }
-} catch(e) {
-    console.log("Out of bounds");
+function fetchUsername() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('JSUser');
+        }, 3000);
+    })
 }
+
+console.log("Fetching username...");
+fetchUsername().then((value) => {
+    console.log(`You are logged in as ${value}`);
+})
+.finally(() => {
+    console.log("Welcome!");
+})
